3a2e2d4807c87f40ef67af2c74e002ac039050ba
fatouuuu <fatmaabdullrahim@gmail.com>
2022-11-30 07:38:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution here
+// // Code your solution here
+     //use callback fn instead
+
+const drivers = ['Bobby', 'SaMMY', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// function findMatching (driverNames, name){
+//     function checkName(item) {
+//         return item.toUpperCase() == name.toUpperCase();
+//     }
+//     return driverNames.filter(checkName);
+// }
+
+
+function findMatching(driverNames,name){
+    let checkDriver = driverNames.filter(item=> item.toUpperCase()=== name.toUpperCase())
+    return checkDriver
+}
+
+
+// function fuzzyMatch (driverNames, firstLetters){
+//     function checkName(item) {
+//         return item.startsWith(firstLetters);
+//     }
+//     return driverNames.filter(checkName);
+// }
+
+
+function fuzzyMatch(driverNames, firstLetters){
+    let checkName = driverNames.filter(item =>item.startsWith(firstLetters))
+    return checkName
+}
+
+
+// function matchName (drivers, name){
+
+
+//     function checkName(driver) {
+//         return driver.name == name;
+//     }
+//     return drivers.filter(checkName);
+// }
+
+
+function matchName (drivers, name){
+    let checkName = drivers.filter(drivers=>drivers.name === name)
+    return checkName
+}
